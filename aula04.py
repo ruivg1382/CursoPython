5469eb9649695b37a9e3421b9b541e75b555a8f0
rui <ruivg1382@gmail.com>
2022-08-10 11:13:28 -0100

    atualização de arquivos

---
+++ aula04.py
@@ -0,0 +1,16 @@
+def alunos(nome,idade):
+    print("Hola",nome)
+
+
+
+def soma(x,y):
+    return x+y
+
+alunos("peter",67)
+
+
+calc=soma(8,9)
+
+print(calc)
+
+
---
+++ aula05.py
@@ -0,0 +1,10 @@
+a, b, c= "maria","carla", "Janisse"
+
+print(a)
+print(b)
+print(c)
+
+itens=["Arroz","Cebola","tomate", "Cenoura", "Pepino"]
+
+for x in range(5):
+    print(itens[x])
